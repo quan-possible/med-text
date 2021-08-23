9d965988197563b2716b68ba6a8d987c31b75e94
Quan Nguyen <nguyenqu@mahti-login1.mahti.csc.fi>
2021-08-23 13:21:24 +0300

    fixes

--- project/base_classifier.py
+++ project/base_classifier.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-import copy
 import logging as log
 from math import ceil
 from argparse import ArgumentParser, Namespace
 from collections import OrderedDict
-
-from label_attention import LabelAttentionLayer
-from tokenizer import Tokenizer
-from datamodule import MedDataModule, Collator
 
 import torch
 import torch.nn as nn
@@ -16,6 +11,8 @@ from transformers import AutoModel
 from abc import abstractmethod
 
 import pytorch_lightning as pl
+from tokenizer import Tokenizer
+from datamodule import MedDataModule, Collator
 from torchnlp.utils import lengths_to_mask
 from pytorch_lightning.utilities.seed import seed_everything
 from utils import mask_fill, get_lr_schedule, str2bool
@@ -46,7 +43,6 @@ class BaseClassifier(pl.LightningModule):
     def num_classes(self):
         pass
 
-
     @abstractmethod
     def _get_metrics(self, logits, labels):
         pass
@@ -74,241 +70,21 @@ class BaseClassifier(pl.LightningModule):
         """
         pass
 
+    @abstractmethod
     def _build_model(self) -> None:
-        """ Init BERT model + tokenizer + classification head."""
-        # pass
+        pass
 
-        self.encoder = AutoModel.from_pretrained(
-            self.hparams.encoder_model, 
-            # hidden_dropout_prob=0.1,
-            output_hidden_states=True,
-        )
-
-        # set the number of features our encoder model will return...
-        if self.hparams.encoder_model == "google/bert_uncased_L-2_H-128_A-2":
-            self.encoder_features = 128
-        else:
-            self.encoder_features = 768
-            
-        #-------------------------
-        # DESCRIPTION EMBEDDINGS
-        #-------------------------
-        
-        # # set the number of features our encoder model will return...
-        # self._label_attn = nn.MultiheadAttention(
-        #     self.encoder_features, self.hparams.num_heads, dropout=0.2,
-        # )
-        
-        # # Classification head
-        # self._classification_head = nn.Sequential(
-        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
-        #     nn.Tanh(),
-        #     nn.Dropout(),
-        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
-        #     nn.Tanh(),
-        #     nn.Linear(self.encoder_features, 1),
-        # )
-        #----------------------------
-        # NO HEAD
-        #----------------------------
-        
-        # # Classification head
-        # self._classification_head = nn.Sequential(
-        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
-        #     nn.Tanh(),
-        #     nn.Dropout(),
-        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
-        #     nn.Tanh(),
-        #     nn.Linear(self.encoder_features, self.num_classes),
-        # )
-        
-        #----------------------------
-        # CONVOLUTIONAL HEAD
-        #----------------------------
-        
-        # filter_sizes = [3, 5, 7]  # Only choose odd numbers
-        # self.dim_conv = int(768 / len(filter_sizes))
-        #
-        # # Conv Network
-        # self.conv1d_list = nn.ModuleList([
-        #     nn.Conv1d(in_channels=encoder_features,
-        #               out_channels=self.dim_conv,
-        #               kernel_size=size,
-        #               padding=floor(size / 2))
-        #     for size in filter_sizes
-        # ])
-        # self.bn1d_list = nn.ModuleList([nn.BatchNorm1d(self.dim_conv) for _ in filter_sizes])
-
-        # # Classification head
-        # self._classification_head = nn.Sequential(
-        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
-        #     nn.Tanh(),
-        #     nn.Dropout(),
-        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
-        #     nn.Tanh(),
-        #     nn.Linear(self.encoder_features, self.num_classes),
-        # )
-        
-        #---------------------------------
-        # STACKED DESCRIPTION EMBEDDINGS
-        #---------------------------------
-        
-        label_attn_layer = LabelAttentionLayer(self.encoder_features, self.hparams.num_heads)
-        self.label_attn = self._get_clones(label_attn_layer, self.hparams.n_lbl_attn_layer)
-        
-        self.classification_head = nn.Sequential(
-            nn.Linear(self.encoder_features, self.encoder_features * 2),
-            nn.Tanh(),
-            nn.Dropout(),
-            nn.Linear(self.encoder_features * 2, self.encoder_features),
-            nn.Tanh(),
-        )
-        self.final_fc = nn.Linear(self.encoder_features,self.num_classes)
-        
-        #---------------------------------
-        # TRANSFORMERS
-        #---------------------------------
-        
-        # trans_block = nn.Transformer(
-        #     d_model=self.encoder_features,
-        #     num_encoder_layers=2, 
-        #     num_decoder_layers=2, 
-        #     dropout=0.2, 
-        #     batch_first=True
-        # )
-        # self._label_attn = self._get_clones(trans_block, self.hparams.n_lbl_attn_layer)
-        
-        # self._classification_head = nn.Sequential(
-        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
-        #     nn.Tanh(),
-        #     nn.Dropout(),
-        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
-        #     nn.Tanh(),
-        # )
-        
-        # self.final_fc = nn.Linear(self.encoder_features,self.num_classes)
-
-    def forward(self, tokens_dict):
+    @abstractmethod
+    def forward(self, tokens_lengths):
         """ Usual pytorch forward function. 
-        :param tokens_dict: tuple of:
+        :param tokens_lengths: tuple of:
             - text sequences [batch_size x src_seq_len]
             - lengths: source lengths [batch_size]
 
         Returns:
             Dictionary with model outputs (e.g: logits)
         """
-        #----------------------------
-        # CONVOLUTIONAL HEAD
-        #----------------------------
-        
-        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
-        # x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
-
-        # x = x.transpose(2, 1)  # (batch_size, hidden_dim, seq_len)
-
-        # x_list = [F.relu(bn1d(conv1d(x))) for conv1d,
-        #           bn1d in zip(self.conv1d_list, self.bn1d_list)]
-
-        # x_list = [F.max_pool1d(x, kernel_size=x.size(2)) for x in x_list]
-
-        # x = torch.cat([x.squeeze(dim=2) for x in x_list], dim=1)
-
-        # logits = self.classification_head(x)
-        
-        #----------------------------
-        # NO HEAD
-        #----------------------------
-        # tokens, lengths = tokens_dict['tokens'], \
-        #     tokens_dict['lengths']
-        # tokens = tokens[:, : lengths.max()]
-
-        # # When using just one GPU this should not change behavior
-        # # but when splitting batches across GPU the tokens have padding
-        # # from the entire original batch
-        # mask = lengths_to_mask(lengths, device=tokens.device)
-
-        # # Run BERT model. output is (batch_size, sequence_length, hidden_size)
-        # word_embeddings = self.encoder(tokens, mask).last_hidden_state
-
-        # # Average Pooling
-        # word_embeddings = mask_fill(
-        #     0.0, tokens, word_embeddings, self.tokenizer.padding_index
-        # )
-        # sentemb = torch.sum(word_embeddings, 1)
-        # sum_mask = mask.unsqueeze(-1).expand(word_embeddings.size()
-        #                                      ).float().sum(1)
-        # sentemb = sentemb / sum_mask
-
-        # # Classification head
-        # logits = self.classification_head(sentemb)
-        
-        #-------------------------
-        # DESCRIPTION EMBEDDINGS WITH GENERAL ATTENTION
-        #-------------------------
-        
-        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
-        # k = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
-
-        # # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
-        # if not self.static_desc_emb:
-        #     self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=k)[:, 0, :].squeeze(dim=1)
-
-        # q = self.desc_emb.clone().type_as(k).expand(k.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
-
-        # # For Multiheadattention. Permuting because batch_size should be in dim=1
-        # # for self.label_attn.
-        # attn_output, _ = self.label_attn(
-        #     q.transpose(1, 0), k.transpose(1, 0), k.transpose(1, 0)
-        # )   # (num_classes, batch_size, hidden_dim)
-
-        # logits = self.classification_head(attn_output).squeeze(dim=2)
-        # logits = logits.transpose(1, 0)    # (batch_size, num_classes)
-        
-        #-------------------------------------------
-        # DESCRIPTION EMBEDDINGS WITH MULTIHEAD BLOCKS
-        #-------------------------------------------
-        
-        # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
-        x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
-        # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
-        if not self.static_desc_emb:
-            self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=x)[:, 0, :].squeeze(dim=1)
-
-        desc_emb = self.desc_emb.clone().type_as(x).expand(x.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
-        
-        # (batch_size, seq_len, hidden_dim)
-        output = desc_emb
-        for mod in self.label_attn:
-            output = mod(x, output)
-
-        output = self.classification_head(output)
-        logits = self.final_fc.weight.mul(output).sum(dim=2).add(self.final_fc.bias)
-        
-        #---------------------------------
-        # TRANSFORMERS
-        #---------------------------------
-        
-        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
-        # x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
-        # # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
-        # if not self.static_desc_emb:
-        #     self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=x)[:, 0, :].squeeze(dim=1)
-            
-        # desc_emb = self.desc_emb.clone().type_as(x)\
-        #     .expand(x.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
-        
-        # # (batch_size, seq_len, hidden_dim)
-        # output = desc_emb
-        # for mod in self.label_attn:
-        #     output = mod(x, output)
-
-        # output = self.classification_head(output)
-        # logits = self.final_fc.weight.mul(output).sum(dim=2).add(self.final_fc.bias)
-
-        return {"logits": logits}
-    
-    def _get_clones(self, module, N):
-        return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+        pass
 
     def unfreeze_encoder(self) -> None:
         """ un-freezes the encoder layer. """
@@ -356,12 +132,12 @@ class BaseClassifier(pl.LightningModule):
         steps_per_epoch = ceil(1303 / (self.hparams.batch_size * 2))
         self.lr_scheduler = get_lr_schedule(
             param_groups=param_groups, encoder_indices=[0], optimizer=self.optimizer,
-            scheduler_epochs=self.hparams.scheduler_epochs, 
+            scheduler_epochs=self.hparams.scheduler_epochs,
             num_frozen_epochs=self.hparams.num_frozen_epochs,
-            steps_per_epoch=steps_per_epoch, 
+            steps_per_epoch=steps_per_epoch,
             warmup_pct=[self.hparams.warmup_pct, self.hparams.warmup_pct],
             smallest_lr_pct=[self.hparams.smallest_lr_pct_encoder,
-                             self.hparams.smallest_lr_pct_lbl_attn, 
+                             self.hparams.smallest_lr_pct_lbl_attn,
                              self.hparams.smallest_lr_pct_nonencoder],
         )
 
@@ -488,7 +264,7 @@ class BaseClassifier(pl.LightningModule):
             type=str,
             help="Averaging methods for validation metrics (micro, macro,...)",
         )
-        
+
         parser.add_argument(
             "--num_heads",
             default=12,
@@ -509,21 +285,21 @@ class BaseClassifier(pl.LightningModule):
             type=int,
             help="Number of epochs the scheduler for the encoder is activated",
         )
-        
+
         parser.add_argument(
             "--weight_decay_encoder",
             default=0.05,
             type=float,
             help="Weight decay for encoder",
         )
-        
+
         parser.add_argument(
             "--weight_decay_nonencoder",
             default=0.1,
             type=float,
             help="Weight decay for non-encoder",
         )
-        
+
         parser.add_argument(
             "--smallest_lr_pct_encoder",
             default=0.01,
@@ -537,14 +313,14 @@ class BaseClassifier(pl.LightningModule):
             type=float,
             help="Smallest non-encoder learning rate being a percentage of the default learning rate",
         )
-        
+
         parser.add_argument(
             "--smallest_lr_pct_nonencoder",
             default=0.4,
             type=float,
             help="Smallest non-encoder learning rate being a percentage of the default learning rate",
         )
-        
+
         parser.add_argument(
             "--n_lbl_attn_layer",
             default=1,
@@ -553,12 +329,12 @@ class BaseClassifier(pl.LightningModule):
         )
 
         parser.add_argument(
-            "--static_desc_emb", 
-            type=str2bool, 
+            "--static_desc_emb",
+            type=str2bool,
             default=False,
             help="Whether to update description embedding using BERT"
         )
-        
+
         parser.add_argument(
             "--label_attn_lr",
             type=float,
@@ -602,7 +378,6 @@ class BaseClassifier(pl.LightningModule):
     #         "val_loss": val_loss_mean,
     #         "val_acc": val_acc_mean,
     #     }
-
 
     #     return result
 if __name__ == "__main__":
--- project/hoc.py
+++ project/hoc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from base_classifier import BaseClassifier
-
+from label_attention import LabelAttentionLayer
 from tokenizer import Tokenizer
 from datamodule import MedDataModule, Collator
 from utils import F1WithLogitsLoss, mask_fill
@@ -40,7 +40,7 @@ class HOCClassifier(BaseClassifier):
         if self.static_desc_emb:
             with torch.no_grad():
                 self.desc_emb = self._process_tokens(self.desc_tokens)[:, 0, :].squeeze(dim=1)
-                
+
         if self.hparams.num_frozen_epochs > 0:
             self.freeze_encoder()
         else:
@@ -50,8 +50,120 @@ class HOCClassifier(BaseClassifier):
     def num_classes(self):
         return self._num_classes
 
+    def _build_model(self) -> None:
+        """ Init BERT model + tokenizer + classification head."""
+        # pass
 
-        
+        self.encoder = AutoModel.from_pretrained(
+            self.hparams.encoder_model,
+            # hidden_dropout_prob=0.1,
+            output_hidden_states=True,
+        )
+
+        # set the number of features our encoder model will return...
+        if self.hparams.encoder_model == "google/bert_uncased_L-2_H-128_A-2":
+            self.encoder_features = 128
+        else:
+            self.encoder_features = 768
+
+        #-------------------------
+        # DESCRIPTION EMBEDDINGS
+        #-------------------------
+
+        # # set the number of features our encoder model will return...
+        # self._label_attn = nn.MultiheadAttention(
+        #     self.encoder_features, self.hparams.num_heads, dropout=0.2,
+        # )
+
+        # # Classification head
+        # self._classification_head = nn.Sequential(
+        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
+        #     nn.Tanh(),
+        #     nn.Dropout(),
+        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
+        #     nn.Tanh(),
+        #     nn.Linear(self.encoder_features, 1),
+        # )
+        #----------------------------
+        # NO HEAD
+        #----------------------------
+
+        # Classification head
+        self.classification_head = nn.Sequential(
+            nn.Linear(self.encoder_features, self.encoder_features * 2),
+            nn.Tanh(),
+            nn.Dropout(),
+            nn.Linear(self.encoder_features * 2, self.encoder_features),
+            nn.Tanh(),
+            nn.Linear(self.encoder_features, self.num_classes),
+        )
+
+        #----------------------------
+        # CONVOLUTIONAL HEAD
+        #----------------------------
+
+        # filter_sizes = [3, 5, 7]  # Only choose odd numbers
+        # self.dim_conv = int(768 / len(filter_sizes))
+        #
+        # # Conv Network
+        # self.conv1d_list = nn.ModuleList([
+        #     nn.Conv1d(in_channels=encoder_features,
+        #               out_channels=self.dim_conv,
+        #               kernel_size=size,
+        #               padding=floor(size / 2))
+        #     for size in filter_sizes
+        # ])
+        # self.bn1d_list = nn.ModuleList([nn.BatchNorm1d(self.dim_conv) for _ in filter_sizes])
+
+        # # Classification head
+        # self._classification_head = nn.Sequential(
+        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
+        #     nn.Tanh(),
+        #     nn.Dropout(),
+        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
+        #     nn.Tanh(),
+        #     nn.Linear(self.encoder_features, self.num_classes),
+        # )
+
+        #---------------------------------
+        # STACKED DESCRIPTION EMBEDDINGS
+        #---------------------------------
+
+        # label_attn_layer = LabelAttentionLayer(self.encoder_features, self.hparams.num_heads)
+        # self.label_attn = self._get_clones(label_attn_layer, self.hparams.n_lbl_attn_layer)
+
+        # self.classification_head = nn.Sequential(
+        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
+        #     nn.Tanh(),
+        #     nn.Dropout(),
+        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
+        #     nn.Tanh(),
+        # )
+        # self.final_fc = nn.Linear(self.encoder_features, self.num_classes)
+
+        #---------------------------------
+        # TRANSFORMERS
+        #---------------------------------
+
+        # trans_block = nn.Transformer(
+        #     d_model=self.encoder_features,
+        #     num_encoder_layers=2,
+        #     num_decoder_layers=2,
+        #     dropout=0.2,
+        #     batch_first=True
+        # )
+        # self._label_attn = self._get_clones(trans_block, self.hparams.n_lbl_attn_layer)
+
+        # self._classification_head = nn.Sequential(
+        #     nn.Linear(self.encoder_features, self.encoder_features * 2),
+        #     nn.Tanh(),
+        #     nn.Dropout(),
+        #     nn.Linear(self.encoder_features * 2, self.encoder_features),
+        #     nn.Tanh(),
+        # )
+
+        # self.final_fc = nn.Linear(self.encoder_features,self.num_classes)
+
     def _build_loss(self):
         # self._loss_fn = nn.BCEWithLogitsLoss(
         #     # pos_weight=torch.tensor([5, 15, 15, 15, 7, 5, 12, 4, 3, 7])
@@ -73,7 +185,7 @@ class HOCClassifier(BaseClassifier):
             normed_logits, labels, num_classes=10, average=self.hparams.metric_averaging)
 
         return acc, f1_, precision_, recall_
-    
+
     def _process_tokens(self, tokens_dict, type_as_tensor=None):
         tokens, lengths = tokens_dict['tokens'], \
             tokens_dict['lengths']
@@ -92,6 +204,128 @@ class HOCClassifier(BaseClassifier):
         emb = self.encoder(tokens, mask).last_hidden_state
 
         return emb
+
+    def _get_clones(self, module, N):
+        return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+    def forward(self, tokens_dict):
+        """ Usual pytorch forward function. 
+        :param tokens_dict: tuple of:
+            - text sequences [batch_size x src_seq_len]
+            - lengths: source lengths [batch_size]
+
+        Returns:
+            Dictionary with model outputs (e.g: logits)
+        """
+        #----------------------------
+        # CONVOLUTIONAL HEAD
+        #----------------------------
+
+        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
+        # x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
+
+        # x = x.transpose(2, 1)  # (batch_size, hidden_dim, seq_len)
+
+        # x_list = [F.relu(bn1d(conv1d(x))) for conv1d,
+        #           bn1d in zip(self.conv1d_list, self.bn1d_list)]
+
+        # x_list = [F.max_pool1d(x, kernel_size=x.size(2)) for x in x_list]
+
+        # x = torch.cat([x.squeeze(dim=2) for x in x_list], dim=1)
+
+        # logits = self.classification_head(x)
+
+        #----------------------------
+        # NO HEAD
+        #----------------------------
+        tokens, lengths = tokens_dict['tokens'], \
+            tokens_dict['lengths']
+        tokens = tokens[:, : lengths.max()]
+
+        # When using just one GPU this should not change behavior
+        # but when splitting batches across GPU the tokens have padding
+        # from the entire original batch
+        mask = lengths_to_mask(lengths, device=tokens.device)
+
+        # Run BERT model. output is (batch_size, sequence_length, hidden_size)
+        word_embeddings = self.encoder(tokens, mask).last_hidden_state
+
+        # Average Pooling
+        word_embeddings = mask_fill(
+            0.0, tokens, word_embeddings, self.tokenizer.padding_index
+        )
+        sentemb = torch.sum(word_embeddings, 1)
+        sum_mask = mask.unsqueeze(-1).expand(word_embeddings.size()
+                                             ).float().sum(1)
+        sentemb = sentemb / sum_mask
+
+        # Classification head
+        logits = self.classification_head(sentemb)
+
+        #-------------------------
+        # DESCRIPTION EMBEDDINGS WITH GENERAL ATTENTION
+        #-------------------------
+
+        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
+        # k = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
+
+        # # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
+        # if not self.static_desc_emb:
+        #     self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=k)[:, 0, :].squeeze(dim=1)
+
+        # q = self.desc_emb.clone().type_as(k).expand(k.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
+
+        # # For Multiheadattention. Permuting because batch_size should be in dim=1
+        # # for self.label_attn.
+        # attn_output, _ = self.label_attn(
+        #     q.transpose(1, 0), k.transpose(1, 0), k.transpose(1, 0)
+        # )   # (num_classes, batch_size, hidden_dim)
+
+        # logits = self.classification_head(attn_output).squeeze(dim=2)
+        # logits = logits.transpose(1, 0)    # (batch_size, num_classes)
+
+        #-------------------------------------------
+        # DESCRIPTION EMBEDDINGS WITH MULTIHEAD BLOCKS
+        #-------------------------------------------
+
+        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
+        # x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
+        # # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
+        # if not self.static_desc_emb:
+        #     self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=x)[:, 0, :].squeeze(dim=1)
+
+        # desc_emb = self.desc_emb.clone().type_as(x).expand(x.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
+
+        # # (batch_size, seq_len, hidden_dim)
+        # output = desc_emb
+        # for mod in self.label_attn:
+        #     output = mod(x, output)
+
+        # output = self.classification_head(output)
+        # logits = self.final_fc.weight.mul(output).sum(dim=2).add(self.final_fc.bias)
+
+        #---------------------------------
+        # TRANSFORMERS
+        #---------------------------------
+
+        # # _process_tokens is defined in BaseClassifier. Simply input the tokens into BERT.
+        # x = self._process_tokens(tokens_dict)  # (batch_size, seq_len, hidden_dim)
+        # # CLS pooling for label descriptions. output shape is (num_classes, hidden_dim)
+        # if not self.static_desc_emb:
+        #     self.desc_emb = self._process_tokens(self.desc_tokens, type_as_tensor=x)[:, 0, :].squeeze(dim=1)
+
+        # desc_emb = self.desc_emb.clone().type_as(x)\
+        #     .expand(x.size(0), self.desc_emb.size(0), self.desc_emb.size(1))
+
+        # # (batch_size, seq_len, hidden_dim)
+        # output = desc_emb
+        # for mod in self.label_attn:
+        #     output = mod(x, output)
+
+        # output = self.classification_head(output)
+        # logits = self.final_fc.weight.mul(output).sum(dim=2).add(self.final_fc.bias)
+
+        return {"logits": logits}
 
     def loss(self, predictions: dict, targets: dict) -> torch.tensor:
         return self._loss_fn(predictions["logits"], targets["labels"].float())
